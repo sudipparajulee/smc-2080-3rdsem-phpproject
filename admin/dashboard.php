0d78e800d5beeaca5ccc717aa00f8741e21c2977
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-04-11 08:33:20 +0545

    updat

---
+++ admin/dashboard.php
@@ -0,0 +1,18 @@
+<?php include 'header.php' ?>
+    <h2 class="text-3xl">Dashboard</h2>
+    <hr class="mb-4 h-1 bg-red-600">
+    <div class="grid grid-cols-3 gap-5">
+        <div class="bg-blue-100 px-4 py-6 rounded-lg shadow-md">
+            <h3 class="text-xl">Total News</h3>
+            <p class="text-3xl font-semibold text-right">100</p>
+        </div>
+        <div class="bg-red-100 px-4 py-6 rounded-lg shadow-md">
+            <h3 class="text-xl">Total Categories</h3>
+            <p class="text-3xl font-semibold text-right">5</p>
+        </div>
+        <div class="bg-green-100 px-4 py-6 rounded-lg shadow-md">
+            <h3 class="text-xl">Total Notices</h3>
+            <p class="text-3xl font-semibold text-right">10</p>
+        </div>
+    </div>
+<?php include 'footer.php' ?>
---
+++ admin/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="flex">
+        <div class="w-52 h-screen bg-gray-100 shadow-md">
+            <h1 class="font-bold text-3xl py-10 text-center">LOGO</h1>
+            <div class="mt-4 grid text-lg">
+                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Dashboard</a>
+                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Notices</a>
+                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Category</a>
+                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">News</a>
+                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Logout</a>
+            </div>
+        </div>
+        <div class="flex-1 p-4">
