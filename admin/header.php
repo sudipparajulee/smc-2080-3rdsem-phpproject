7966f6a8f21eefc00ea83c2a8aa52aab167fabcb
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-04-15 09:06:06 +0545

    notice index

--- admin/header.php
+++ admin/header.php
@@ -11,8 +11,8 @@
         <div class="w-52 h-screen bg-gray-100 shadow-md">
             <h1 class="font-bold text-3xl py-10 text-center">LOGO</h1>
             <div class="mt-4 grid text-lg">
-                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Dashboard</a>
-                <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Notices</a>
+                <a href="dashboard.php" class="pl-4 py-2 border-b hover:bg-gray-200">Dashboard</a>
+                <a href="notices.php" class="pl-4 py-2 border-b hover:bg-gray-200">Notices</a>
                 <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Category</a>
                 <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">News</a>
                 <a href="" class="pl-4 py-2 border-b hover:bg-gray-200">Logout</a>
---
+++ admin/notices.php
@@ -0,0 +1,19 @@
+<?php include 'header.php' ?>
+    <h2 class="text-3xl">Notices</h2>
+    <hr class="mb-4 h-1 bg-red-600">
+    <div class="flex mb-4 justify-end">
+        <a href="" class="bg-blue-600 text-white px-4 py-2 rounded-lg shadow-md">Add Notice</a>
+    </div>
+    <table class="w-full">
+        <tr class="bg-gray-200">
+            <th class="p-2 border border-gray-300">S.N.</th>
+            <th class="p-2 border border-gray-300">Notice</th>
+            <th class="p-2 border border-gray-300">Action</th>
+        </tr>
+        <tr class="text-center">
+            <td class="p-2 border">1</td>
+            <td class="p-2 border">This is a notice</td>
+            <td class="p-2 border">Edit Delete</td>
+        </tr>
+    </table>
+<?php include 'footer.php' ?>
