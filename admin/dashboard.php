4c780b6e1408f7b0201595185721d3f6d29a112c
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-04-29 06:54:30 +0545

    Count Category and Notices

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,4 +1,13 @@
-<?php include 'header.php' ?>
+<?php include 'header.php';
+$qrycat = "SELECT COUNT(id) as total_cat FROM categories";
+$qrynotice = "SELECT COUNT(id) as total_notice FROM notices";
+include 'dbconnection.php';
+$resultcat = mysqli_query($conn,$qrycat);
+$resultnotice = mysqli_query($conn,$qrynotice);
+$rowcat = mysqli_fetch_assoc($resultcat);
+$rownotice = mysqli_fetch_assoc($resultnotice);
+include 'closeconnection.php';
+?>
     <h2 class="text-3xl">Dashboard</h2>
     <hr class="mb-4 h-1 bg-red-600">
     <div class="grid grid-cols-3 gap-5">
@@ -8,11 +17,11 @@
         </div>
         <div class="bg-red-100 px-4 py-6 rounded-lg shadow-md">
             <h3 class="text-xl">Total Categories</h3>
-            <p class="text-3xl font-semibold text-right">5</p>
+            <p class="text-3xl font-semibold text-right"><?php echo $rowcat['total_cat'];?></p>
         </div>
         <div class="bg-green-100 px-4 py-6 rounded-lg shadow-md">
             <h3 class="text-xl">Total Notices</h3>
-            <p class="text-3xl font-semibold text-right">10</p>
+            <p class="text-3xl font-semibold text-right">3</p>
         </div>
     </div>
 <?php include 'footer.php' ?>
